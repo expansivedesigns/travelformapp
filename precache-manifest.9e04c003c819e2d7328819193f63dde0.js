95e1cdb58d2ad7567872d27bf9d1c2ca8198717f
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2019-12-14 20:23:41 -0500

    Updates

---
+++ precache-manifest.9e04c003c819e2d7328819193f63dde0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46775b99bd6c79686e1246cd38d285a6",
+    "url": "/travelformapp/index.html"
+  },
+  {
+    "revision": "3c6872fa83102bf5d30c",
+    "url": "/travelformapp/static/css/main.1d71a774.chunk.css"
+  },
+  {
+    "revision": "0dc0db5ad30fd180dec7",
+    "url": "/travelformapp/static/js/2.3b447154.chunk.js"
+  },
+  {
+    "revision": "3c6872fa83102bf5d30c",
+    "url": "/travelformapp/static/js/main.7f3411b1.chunk.js"
+  },
+  {
+    "revision": "80dd4f9b8e374d9bbf0f",
+    "url": "/travelformapp/static/js/runtime-main.e0bf53bd.js"
+  }
+]);
